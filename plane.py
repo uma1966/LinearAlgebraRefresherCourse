41e5cdc3e78838ba58a061af8319d3ce9d0c2a50
Uwe Maurer <u.maurer@enbw.com>
2017-08-04 15:59:25 +0200

    Lesson 3, Intersections, Quiz 7 solved.

--- plane.py
+++ plane.py
@@ -96,6 +96,67 @@ class Plane(object):
                 return k
         raise Exception(Plane.NO_NONZERO_ELTS_FOUND_MSG)
 
+    def parallel_to(self, other):
+        # planes are parellel if their normal vectors are parallel
+        result = self.normal_vector.is_parallel_to(other.normal_vector)
+        return result
+
+    def equal_to(self, other):
+        # Check special case normal vector is the zero vector:
+        if self.normal_vector.is_zero():
+            if not other.normal_vector.is_zero():
+                return False
+            else:
+                # coefficients of equation are zero --> planes are
+                # equal if constant terms are equal
+                diff = self.constant_term - other.constant_term
+                return MyDecimal(diff).is_near_zero()
+        elif other.normal_vector.is_zero():
+            return False
+
+        if not self.parallel_to(other):
+            # non parallel planes can't be equal
+            return False
+        # Check if vector between one point on each line is orthogonal to the planes normal vectors
+        # First connect the basepoints:
+        connecting_vector = self.basepoint.sub(other.basepoint)
+        # Then check orthogonality to normal vector (as the planes
+        # are parallel it is sufficient to check with just one normal vector):
+        orthogonal = connecting_vector.is_orthogonal_to(self.normal_vector)
+        return orthogonal
+
+    def __eq__(self, other):
+        return self.equal_to(other)
+
+    def intersection(self, other):
+        """Intersection for 3 dimensions"""
+        if self.dimension != 3:
+            raise Exception("intersection implemented for 3 dimensions only")
+        if self.parallel_to(other):
+            if self.equal_to(other):
+                # equal planes have an indefinite number of intersections,
+                # so we return the plane itself:
+                return self
+            else:
+                # parallel planes have no intersection
+                return None
+        else:
+            return "not parallel"
+        # else calculate x,y,z for equation system, assuming
+        # A and B are not 0 at the same time (this is the case because
+        # they are not parallel). Equation system is:
+        #  A*x + B*y + C*z = k1
+        #  D*x + E*y + F*z = k2
+        """
+        A,B,C = self.normal_vector.coordinates
+        k1 = self.constant_term
+        D,E,F = other.normal_vector.coordinates
+        k2 = other.constant_term
+        x = 0
+        y = 0
+        return Vector((x,y))
+        """
+
 
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
