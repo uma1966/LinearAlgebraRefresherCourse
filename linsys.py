d0f4b6953d01830c321d82649930f82fad217964
uwe <uwe@familie-maurer.eu>
2017-08-28 15:39:12 +0200

    Ported linsys.py to Python 3.

--- linsys.py
+++ linsys.py
@@ -96,13 +96,13 @@ p3 = Plane(normal_vector=Vector(['1','0','-2']), constant_term='2')
 
 s = LinearSystem([p0,p1,p2,p3])
 
-print s.indices_of_first_nonzero_terms_in_each_row()
-print '{},{},{},{}'.format(s[0],s[1],s[2],s[3])
-print len(s)
-print s
+print(s.indices_of_first_nonzero_terms_in_each_row())
+print('{},{},{},{}'.format(s[0],s[1],s[2],s[3]))
+print(len(s))
+print(s)
 
 s[0] = p1
-print s
+print(s)
 
-print MyDecimal('1e-9').is_near_zero()
-print MyDecimal('1e-11').is_near_zero()
+print(MyDecimal('1e-9').is_near_zero())
+print(MyDecimal('1e-11').is_near_zero())
